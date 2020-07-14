2d22494c87e10563ff0353ec1eee6995c66605c2
Georg vom Endt <gve@3yourmind.com>
2020-07-14 12:59:12 +0200

    econd commit

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/krysopath/golang-cookie-cutter.git
 
 go 1.14
+
+require gopkg.in/yaml.v2 v2.3.0
--- main.go
+++ main.go
@@ -1,25 +1,86 @@
 package main
 
-type Ingredient struct {
-	Name string
-	ID   int64
+import (
+	"fmt"
+	"time"
+
+	"gopkg.in/yaml.v2"
+)
+
+type (
+	// Ingredient is needed for Cookie
+	Ingredient struct {
+		ID   int
+		Name string
+	}
+	// Cookie is delicious treat
+	Cookie struct {
+		Ingredients []Ingredient `yaml:"ingredients"`
+	}
+	// Oven interface is requirement for baked cookie
+	Oven interface {
+		Store(Cookie)
+		Bake(time.Duration)
+		Get() []Cookie
+	}
+	// Our fancy selfmade Oven, with private Property
+	CookieCutterOven struct {
+		Storage         []Cookie
+		privateProperty bool
+	}
+)
+
+// PrepareCookie preapres a delicious cookie
+func PrepareCookie(ingredients []Ingredient) Cookie {
+	if len(ingredients) < 1 {
+		panic(("No ingredients, how to make a cookie?"))
+	}
+	return Cookie{Ingredients: ingredients}
 }
 
-type IngredientList struct{}
+func (cc CookieCutterOven) Store(cookie Cookie) {
+	cc.Storage = append(cc.Storage, cookie)
+}
 
-type Cookie interface {
-	Bake([]Ingredient) (int64, bool)
-	Eat() (bool, error)
+func (cc CookieCutterOven) Bake(duration time.Duration) {
+	time.Sleep(duration)
+	fmt.Println("Ding Ding, cookies are done!")
+}
+
+func (cc CookieCutterOven) Get() []Cookie {
+	return cc.Storage
 }
 
 func main() {
-	delicious := []Ingredient{
-		Ingredient{Name: "Sugar"},
-		Ingredient{Name: "Flour"},
-		Ingredient{Name: "Milk"},
-		Ingredient{Name: "Egg"},
+	var delicious = []Ingredient{
+		Ingredient{Name: "Sugar", ID: 0},
+		Ingredient{Name: "Flour", ID: 1},
+		Ingredient{Name: "Milk", ID: 2},
+		Ingredient{Name: "Egg", ID: 3},
 	}
+	//fmt.Printf("%+v\n", delicious)
+	//duration := 5 * time.Second
 
-	var cookie Cookie
+	var cookie1 Cookie
+	var cookie2 Cookie
+	var cookie3 Cookie
+	var cookieBaker Oven
+
+	cookie1 = PrepareCookie(delicious)
+	cookie2 = PrepareCookie(delicious)
+	cookie3 = PrepareCookie(delicious)
+	cookieBaker = CookieCutterOven{}
+
+	//_ = cookieCreator.(Oven)
+
+	cookieBaker.Store(cookie1)
+	cookieBaker.Store(cookie2)
+	cookieBaker.Store(cookie3)
+
+	ovenBytes, ymlErr := yaml.Marshal(cookieBaker)
+	if ymlErr != nil {
+		panic(ymlErr)
+	}
+	fmt.Println(string(ovenBytes))
 
 }
